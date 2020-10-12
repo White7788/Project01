682fde5317b198d77de50203e3ad44e6ff399afe
unknown <Administrator@SHA001.corp.com>
2020-10-12 17:29:00 +0800

    Initial commit

---
+++ Module01/src/com/java/bean/Customer.java
@@ -0,0 +1,12 @@
+package com.java.bean;
+
+public class Customer {
+    public static void main(String[] args) {
+
+
+        System.out.println("hello world");
+        System.out.println("hello world");
+
+
+    }
+}
